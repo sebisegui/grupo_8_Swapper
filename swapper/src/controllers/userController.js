2b457a1d35af3da9b301f9715861713e19879118
MiltonChico <milton.chico@outlook.com>
2020-06-14 12:10:27 -0300

    se modifican todos los archivos dejandolos ordenados, se crea la logica para obtener datos desde una base de datos

--- swapper/src/controllers/userController.js
+++ swapper/src/controllers/userController.js
@@ -1,27 +1,58 @@
+// REQUIRES
 var express = require('express');
 var router = express.Router();
+const path = require ('path')
 const fs = require('fs');
 
-const userController ={
-    register: (req,res) =>{
-        res.render('register')
-    },
 
+//ACCESO A DATA BASE
+const usersPath = path.join(__dirname, '../database/usersDataBase.json')
+const productsPath = path.join(__dirname, '../database/productsDataBase.json')
+const listaUsuarios = fs.readFileSync(usersPath)
+const listaProductos = fs.readFileSync(productsPath)
+
+// VARIABLES A UTILIZAR 
+let listaUsuariosJS = JSON.parse(listaUsuarios)
+let listaProductosJS = JSON.parse(listaProductos)
+
+// CONTROLLER
+const userController ={  
     login: (req,res) =>{
         res.render('login')
     },
-
+    index: (req, res) =>{
+        res.render('index', {listaProductosJS})
+    },
+    detalleProduct: (req,res) =>{
+        let producto = listaProductosJS.find(producto => producto.id == req.params.id)
+        res.render('detalle', {producto: producto})
+    },
+    cargaProduct: (req, res) =>{
+        res.render('formulario-carga')
+    },
+    edit: (req,res) =>{
+        let productToEdit = listaProductosJS.find(product => producto.id == req.params.id)
+        res.render('edit-form', {productToEdit})
+    },
+    storeNew: (req,res) =>{
+        res.render('index')
+    },
+    update: (req,res) =>{
+        res.render('index')
+    },
+    delete: (req,res) => { 
+        let deleteId = req.params.id
+        let newDataBase = listaProductosJS.filter(product => product.id != deleteId)
+        let newDataBaseJS = JSON.stringify(newDataBase, null, " ");
+        fs.writeFileSync(productsPath, newDataBaseJS);
+        res.render('index')
+    },
+    register: (req,res) =>{
+        res.render('register')
+    },
     mensajes: (req,res) =>{
         res.render('mensajes')
-    },
-    detalle: (req,res) =>{
-        res.render('detalle')
-    },
-    
-    formulario: (req, res) =>{
-        res.render('formulario-carga')
     }
-    
 }
 
 module.exports = userController
--- swapper/src/routes/index.js
+++ swapper/src/routes/index.js
@@ -3,19 +3,39 @@ var router = express.Router();
 var userController = require('../controllers/userController')
 
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// HOME LOGIN
+router.get('/',userController.login);
+
+// LISTA PRODUCTOS
+
+router.get('/products', userController.index);
+
+// FORMULARIO CARGA PRODUCTO
+
+router.get('/products/create', userController.cargaProduct);
+router.post('/products/create', userController.storeNew)
+
+// DETALLE DE PRODUCTO
+
+router.get('/products/:id', userController.detalleProduct);
+
+// FORMULARIO EDIT PRODUCTO
+
+router.get('/products/:id/edit', userController.edit)
+router.put('/products/:id', userController.update)
+
+// ELIMINAR PRODUCTO
+
+router.delete('/products/:id', userController.delete)
+
+// FORMULARIO REGISTRO
 
 router.get('/register',userController.register);
 
-router.get('/login',userController.login);
+// SECCION MENSAJES
 
 router.get('/mensajes',userController.mensajes);
 
-router.get('/detalle', userController.detalle);
 
-router.get('/cargar', userController.formulario);
 
 module.exports = router;
