c1e4ef6c3c80d081c6b5f9b6da6b7fd98ec4cb30
MiltonChico <milton.chico@outlook.com>
2020-06-20 21:41:56 -0300

    creado formulario registro con modificacion en base de datos, + multer y bcrypt , y detalles

--- swapper/src/app.js
+++ swapper/src/app.js
@@ -4,6 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const methodOverride = require('method-override');
+const multer = require('multer');
+const session = require('express-session');
+const { check, validationResult } = require('express-validator');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,6 +24,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(methodOverride('_method'));
+// app.use(session( {secret: "Nuestro mensaje secreto"}));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- swapper/src/controllers/userController.js
+++ swapper/src/controllers/userController.js
@@ -3,16 +3,18 @@ var express = require('express');
 var router = express.Router();
 const path = require ('path')
 const fs = require('fs');
+const bcrypt = require('bcryptjs')
+const session = require('express-session');
+const { check, validationResult } = require('express-validator');
 
 
 //ACCESO A DATA BASE
 const usersPath = path.join(__dirname, '../database/usersDataBase.json')
 const productsPath = path.join(__dirname, '../database/productsDataBase.json')
-const listaUsuarios = fs.readFileSync(usersPath)
 const listaProductos = fs.readFileSync(productsPath)
 
 // VARIABLES A UTILIZAR 
-let listaUsuariosJS = JSON.parse(listaUsuarios)
+let listaUsuariosJS = JSON.parse(fs.readFileSync(usersPath, 'utf-8'))
 let listaProductosJS = JSON.parse(listaProductos)
 
 // CONTROLLER
@@ -20,20 +22,28 @@ const userController ={
     login: (req,res) =>{
         res.render('login')
     },
+    validationLogin : (req,res) =>{
+        res.redirect('/')
+    },
+    //TODOS LOS PRODUCTOS
     index: (req, res) =>{
         res.render('index', {listaProductosJS});
     },
+    //DETALLE DE PRODUCTO CON SU ID
     detalleProduct: (req,res) =>{
         let producto = listaProductosJS.find(producto => producto.id == req.params.id)
         res.render('detalle', {producto: producto,listaProductosJS})
     },
+    //FORMULARIO DE CARGA DE PRODUCTO
     cargaProduct: (req, res) =>{
         res.render('formulario-carga')
     },
+    //EDITAR UN PRODUCTO CON SU ID
     edit: (req,res) =>{
         let productToEdit = listaProductosJS.find(producto => producto.id == req.params.id)
         res.render('edit-form', {producto:productToEdit})
     },
+    //CARGAR UN PRODUCTO EN LA BASE DE DATOS
     store: (req,res) =>{
         let storeProduct ={
             id: listaProductosJS[listaProductosJS.length-1].id+1,
@@ -48,7 +58,9 @@ const userController ={
         listaProductosJS.push(storeProduct);
         fs.writeFileSync(productsPath,JSON.stringify(listaProductosJS))
             res.redirect('/products/')
-    },
+
+    },     
+    //ACTUALIZAR PRODUCTO
     update: (req,res) =>{
         let idProducto = req.params.id;
         listaProductosJS.forEach(producto => {
@@ -62,9 +74,9 @@ const userController ={
                 producto.imagen = req.body.imagen
             }});
             fs.writeFileSync(productsPath,JSON.stringify(listaProductosJS))
-            res.redirect('/products/')
-    },
-
+            res.redirect('/products/')          
+    },       
+    //ELIMINAR PRODUCTO 
     delete: (req,res) => { 
         let deleteId = req.params.id;
         listaProductosJS.forEach(producto => {
@@ -75,10 +87,33 @@ const userController ={
             fs.writeFileSync(productsPath,JSON.stringify(listaProductosJS))
             res.redirect('/products/')
     },
+    //FORMULARIO DE REGISTRO
     register: (req,res) =>{
+    
         res.render('register')
     },
-    mensajes: (req,res) =>{
+    //GUARDAR UN USUARIO EN BASE DE DATOS
+
+    //Metodo para guardar un Usuario al momento de Registrarse, modificando la DB
+    userStore : (req,res,next) =>{
+        userStore = {
+            //Estos campos los completo con el mismo nombre de la base de datos y obtengo info desde el formulario
+            id: listaUsuariosJS[listaUsuariosJS.length - 1].id + 1,
+            usuario_nombre: req.body.usuario_nombre,
+            usuario_dni: req.body.usuario_dni,
+            usuario_tag : req.body.usuario_tag,
+            email : req.body.email,
+            //Contraseña encriptada con BCRYPT
+            usuario_contraseña1: bcrypt.hashSync(req.body.usuario_contraseña1, 15),
+            usuario_contraseña2: bcrypt.hashSync(req.body.usuario_contraseña2, 15),
+            avatar:req.files[0].filename,
+        }
+        let newDB = [...listaUsuariosJS, userStore]
+        fs.writeFileSync(usersPath, JSON.stringify(newDB, null, ' '))
+        res.send(newDB) 
+    },
+    //CHAT MENSAJES
+    mensajes: (req,res)=>{
         res.render('mensajes')
     }
 }
